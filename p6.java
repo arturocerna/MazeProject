fd5f767d3554bf17ac82b48ce897be7128a6fd87
arturocerna <51428393+arturocerna@users.noreply.github.com>
2019-07-17 13:45:42 -0700

    Now Displays multiple mazes running program once

--- p6.java
+++ p6.java
@@ -1,20 +1,28 @@
 import java.util.Scanner;
 import javax.swing.*;
-import java.awt.image.BufferedImage;
 public class p6 {
 	public static void main(String[] args)
 	{
 		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number to set dimension of maze.");
+		System.out.println("To Exit, Enter a non-integer.");
+		JFrame frame = new JFrame();
+		JLabel contents = new JLabel();
 		try {
-			int size = input.nextInt();
-			mazeGenerator maze = new mazeGenerator(size);
-		    DisplayMaze mazeImg = new DisplayMaze(size, maze.getMazeArray());
-		    JFrame frame = new JFrame();
-		    frame.getContentPane().add(new JLabel(new ImageIcon(mazeImg.img)));
-		    frame.pack();
-		    frame.setVisible(true);
+			while(input.hasNextInt()) {
+				frame.setVisible(false);
+				int size = input.nextInt();
+				mazeGenerator maze = new mazeGenerator(size);
+				DisplayMaze mazeImg = new DisplayMaze(size, maze.getMazeArray());
+				contents = new JLabel(new ImageIcon(mazeImg.img));
+				frame.getContentPane().removeAll();
+				frame.getContentPane().add(contents);
+				frame.pack();
+				frame.setVisible(true);
+			}
 		} finally {
 			input.close();
 		}
+		System.exit(0);
 	}
 }
